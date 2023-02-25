5d0ebdb1030c2c9686956b5e5c366f9b5113346b
angeluskings04 <castro_angel@ciencias.unam.mx>
2023-02-24 21:47:01 -0600

    Se empezo a implemetantar Cajon.java

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Tarea01
-
+# Tarea01 
+ 
+<br> 
+ 
+|Integrantes del equipo| 
+|------------------| 
+|Castro Reyes Angel           Miranda Ruiz Brenda| 
 ### Aqui iria mi programa, si tan solo tuviera listo ya.
 
 ![This is an image](https://cdn.memes.com/stockphotos/This-Is-Where-Id-Put-My-Trophy-If-I-Had-One.jpg)
---
+++ src/Cajon.java
@@ -0,0 +1,38 @@
+import java.time.LocalTime;
+
+/**
+ * Tarea 01
+ * Clase Cajon.
+ * 
+ * @author Miranda Ruiz.
+ * @author Castro Reyes Angel.
+ * @version 1.0
+ */
+
+public class Cajon {
+    /*
+     * Atributos que posee el cajon de estacionamiento.
+     */
+    boolean ocupado; // Si esta ocupado o no.
+    int horaEntrada; // Hora en la que entro.
+    int horaSalida; // Hora en la que se esta saliendo
+    LocalTime ltim;
+
+    // Constructor por omision.
+    public Cajon() {
+        ocupado = false;
+    }
+
+    // Metodo que cambia el valor de horaEntrada usando el metodo de
+    // java.time.LocalTime.
+    public void registrarEntrada() {
+        horaEntrada = ltim.getHour();
+
+    }
+
+    // Metodo que cambia el valor de horaSalida usando el metodo de
+    // java.time.LocalTime.
+    public void registrarSalida() {
+        horaSalida = ltim.getHour();
+    }
+}
