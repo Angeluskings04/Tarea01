33a519f4133b28274e7a7804783bd88539fcca3d
brenda <brendamedina03@ciencias.unam.mx>
2023-02-27 10:14:06 -0600

    avance

--- src/Ticket.java
+++ src/Ticket.java
@@ -2,9 +2,19 @@
 
 public class Ticket{
   public static void main(String[] args) {
-    
+
     Vehiculo c1 = new Vehiculo("CARRO", "Volkswagen","Golf", 2016, "NEGRO","AEF142");
 
     System.out.println(c1);
+    System.out.println("  ______");
+    System.out.println(" /|_||_\\`.__");
+    System.out.println("(   _    _ _\\");
+    System.out.println("=`-(_)--(_)-' ");
+
+    Vehiculo m1 = new Vehiculo("MOTOCICLETA", "Ducati","Monster", 2010, "ROJA","DFE878");
+    System.out.println("   ,");
+    System.out.println(".-/c-.,::");
+    System.out.println("(_)'==(_)");
   }
+
 }
