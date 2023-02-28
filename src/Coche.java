b3ebaaba03c9b11028c08e0b51c0db007fbd227d
brenda <brendamedina03@ciencias.unam.mx>
2023-02-27 20:31:40 -0600

    correccion

--- src/Coche.java
+++ src/Coche.java
@@ -1,34 +1,13 @@
 
 public class Coche extends Vehiculo{
 
-  public static enum TipoCoche {
-    HIBRIDO, ELECTRICO, CLASICO;
-  }
-   TipoCoche tipoCoche;
 
-
-  public Coche(String marca, String modelo, int año, Color color, String placas,  String st){
-    super();
-    switch (st){
-      case "HIBRIDO":
-        tipoCoche = TipoCoche.HIBRIDO;
-        break;
-      case "ELECTRICO":
-        tipoCoche = TipoCoche.ELECTRICO;
-        break;
-      case "CLASICO":
-        tipoCoche = TipoCoche.CLASICO;
-        break;
-      default:
-        tipoCoche = TipoCoche.CLASICO;
-        break;
-    }
+  public Coche(String marca, String modelo, int año, Color color, String placas,  Tipo tipo){
+    super(marca, modelo, año, color, placas, tipo);
   }
 
-  public String impCoche(){
-    if(pension == false){
-    return "Marca:" + marca + "\n" + "Modelo:" + modelo + "\n" + "Año:" + año + "\n" + "Placas:" + placas;
-  }else {return "Marca:" + marca + "\n" + "Modelo:" + modelo + "\n" + "Año:" + año + "\n" + "Placas:" + placas + "\n" + "Tipo de coche:" + tipoCoche;
-    }
+  public void impCoche(){
+    super.ilustracion();
+    System.out.println("Marca:" + marca + "\n" + "Modelo:" + modelo + "\n" + "Año:" + año + "\n" + "Placas:" + placas + "\n" + "Tipo de coche:" + tipo);
   }
 }
